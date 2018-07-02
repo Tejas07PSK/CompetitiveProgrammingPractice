2b6a43b817aa81a73bfe43f906d3b2abb4051970
Palash Sarkar <palashsarkar0007@gmail.com>
2018-07-03 01:17:23 +0530

    Simple java program, to add two numbers!!

---
+++ HackerEarth/src/com/hackerearth/beginner/AddTwoNos.java
@@ -0,0 +1,26 @@
+/*
+
+        @Author : Tejas07PSK (Palash Sarkar),
+        @CreatedON : 2 JUL, 2018, 1:47:53 PM
+        @File-Name : PrintName.java
+
+ */
+
+package com.hackerearth.beginner;
+
+import java.util.Scanner;
+
+class AddTwoNos
+{
+
+    public static void main(String[] args)
+    {
+
+        Scanner s = new Scanner(System.in);
+        int sum = s.nextInt() + s.nextInt();
+        System.out.println(sum);
+        s.close();
+
+    }
+
+}
