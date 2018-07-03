8ce0b8dcb1cbfa448fb2c747fcd163e6e6dd98ad
Palash Sarkar <palashsarkar0007@gmail.com>
2018-07-03 15:01:24 +0530

    Simple java program, to add four numbers!!

---
+++ HackerEarth/src/com/hackerearth/beginner/SumOfFour.java
@@ -0,0 +1,24 @@
+package com.hackerearth.beginner;
+
+import java.util.Scanner;
+
+public class SumOfFour
+{
+
+    public static void main(String[] args)
+    {
+
+        int [] numArray = new int[4];
+        Scanner s = new Scanner(System.in);
+        int sum = 0;
+        for(int i = 0; i < 4; i++){
+
+            numArray[i] = s.nextInt();
+            sum += numArray[i];
+        }
+        System.out.print(sum);
+        s.close();
+
+    }
+
+}
