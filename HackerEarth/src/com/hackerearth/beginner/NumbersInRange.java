3b8a0a1708ef2208f09a9341bbaed67f3dc558f6
Palash Sarkar <palashsarkar0007@gmail.com>
2018-07-04 23:52:21 +0530

    Simple java program to print numbers in range!!

--- HackerEarth/src/com/hackerearth/beginner/NumbersInRange.java
+++ HackerEarth/src/com/hackerearth/beginner/NumbersInRange.java
@@ -1,3 +1,11 @@
+/*
+
+        @Author : Tejas07PSK (Palash Sarkar),
+        @CreatedON : 4 JUL, 2018, 1:47:53 AM
+        @File-Name : NumbersInRange.java
+
+ */
+
 package com.hackerearth.beginner;
 
 import java.util.Scanner;
@@ -9,7 +17,18 @@ public class NumbersInRange
     {
 
         Scanner s = new Scanner(System.in);
-        for(int i = s.nextInt(); i <= s.nextInt(); i++)
+        int L = 0;
+        int R = 0;
+        while (true)
+        {
+
+            L = s.nextInt();
+            R = s.nextInt();
+            if (((L >= 1) && (L <= 100)) && ((R >= 1) && (R <= 100)))
+                break;
+
+        }
+        for(int i = L; i <= R; i++)
             System.out.print(i + " ");
         System.out.println();
         s.close();
