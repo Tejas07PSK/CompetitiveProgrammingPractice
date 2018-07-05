d66166a7ef70ccb188182752660ad0cb92e2c02e
Palash Sarkar <palashsarkar0007@gmail.com>
2018-07-06 02:29:36 +0530

    Basics Of I/O 'Palindromic String'!!

---
+++ HackerEarth/src/com/hackerearth/codemonk/bsio/PalindromicString.java
@@ -0,0 +1,30 @@
+/*
+
+        @Author : Tejas07PSK (Palash Sarkar),
+        @CreatedON : 06 Jul, 2018, 2:16 AM,
+        @File-Name : PalindromicString.java
+
+ */
+
+package com.hackerearth.codemonk.bsio;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.StringBuilder;
+
+public class PalindromicString
+{
+
+    public static void main(String args []) throws Exception
+    {
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringBuilder sb = new StringBuilder((br.readLine()).toLowerCase());
+            if ((sb.toString()).equalsIgnoreCase((sb.reverse()).toString()))
+                System.out.println("YES");
+            else
+                System.out.println("NO");
+
+    }
+
+}
