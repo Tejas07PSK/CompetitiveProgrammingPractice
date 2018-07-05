8490af8edad2baf2f122bd69c374845754644136
Palash Sarkar <palashsarkar0007@gmail.com>
2018-07-05 13:42:32 +0530

    Basics Of I/O starter problem!!

---
+++ HackerEarth/src/com/hackerearth/codemonk/bsio/FormatPrinting.java
@@ -0,0 +1,26 @@
+/*
+
+        @Author : Tejas07PSK (Palash Sarkar),
+        @CreatedON : 4 JUL, 2018, 1:47:53 PM
+        @File-Name : FormatPrinting.java
+
+ */
+
+package com.hackerearth.codemonk.bsio;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class FormatPrinting
+{
+
+    public synchronized static void main(String [] args) throws Exception
+    {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println((Integer.parseInt(br.readLine()) * 2));
+        System.out.println(br.readLine());
+
+    }
+
+}
