f9d9bf62b6380dd7add95dc69b93b98b72f46937
Palash Sarkar <palashsarkar0007@gmail.com>
2018-07-05 20:42:29 +0530

    Basics Of I/O 'Prime Number'!!

---
+++ HackerEarth/src/com/hackerearth/codemonk/bsio/PrimeNo.java
@@ -0,0 +1,54 @@
+/*
+
+        @Author : Tejas07PSK (Palash Sarkar),
+        @CreatedON : 05 Jul, 2018, 6:50 PM,
+        @File-Name : PrimeNo.java
+
+ */
+
+package com.hackerearth.codemonk.bsio;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.Math;
+
+public class PrimeNo
+{
+
+    public synchronized static void main(String args[]) throws Exception
+    {
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int N = Integer.valueOf(br.readLine());
+            for (int i = 2; i <= N; i = i + 1)
+            {
+
+                int count = 2;
+                boolean flag = true;
+                while (count <= Math.sqrt(i))
+                {
+
+                    if ((i % count) == 0)
+                    {
+
+                        flag = false;
+                        break;
+
+                    }
+                    count += 1;
+
+                }
+                if (flag)
+                {
+
+                    System.out.print(i);
+                    if (i != N)
+                        System.out.print(" ");
+
+                }
+
+            }
+
+    }
+
+}
