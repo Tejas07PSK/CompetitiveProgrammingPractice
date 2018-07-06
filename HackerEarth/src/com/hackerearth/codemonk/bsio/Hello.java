711ce9dbc40502f40cb0f7ba9f70b522f663fda4
Palash Sarkar <palashsarkar0007@gmail.com>
2018-07-07 01:47:43 +0530

    Basics Of I/O 'Hello'!!

---
+++ HackerEarth/src/com/hackerearth/codemonk/bsio/Hello.java
@@ -0,0 +1,21 @@
+/*
+
+        @Author : Tejas07PSK (Palash Sarkar),
+        @CreatedON : 07 Jul, 2018, 1:42 AM,
+        @File-Name : Hello.java
+
+ */
+
+package com.hackerearth.codemonk.bsio;
+
+public class Hello
+{
+
+    public static void main(String [] args) throws Exception
+    {
+
+        System.out.println("Hello Kirti");
+
+    }
+
+}
