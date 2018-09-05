6c6a3474181eff56ff799f43903a87f7497cee1a
Palash Sarkar <palashsarkar0007@gmail.com>
2018-09-06 00:17:08 +0530

    LeetCode 'Arrays' problem 'PlusOne.java' !!

---
+++ InterviewBit/src/com/ib/programming/arrays/PlusOne.java
@@ -0,0 +1,46 @@
+/*
+
+        @Author : Tejas07PSK (Palash Sarkar),
+        @CreatedON : 06 Sep, 2018, 12:14 AM,
+        @File-Name : PlusOne.java
+
+ */
+
+package com.ib.programming.arrays;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+
+public class PlusOne
+{
+
+    public int[] plusOne(int[] digits)
+    {
+
+        ArrayList <Integer> arr_temp = new ArrayList <Integer> ();
+        int carry = 1;
+        for(int i = digits.length - 1; i >= 0; i -= 1)
+        {
+
+            arr_temp.add(0, ((digits[i] + carry) % 10));
+            carry =  ((digits[i] + carry) / 10);
+
+        }
+        if ( carry != 0)
+        {
+
+            arr_temp.add(0, carry);
+
+        }
+        int arr [] = new int [arr_temp.size()];
+        for(int i = 0; i < arr_temp.size(); i += 1)
+        {
+
+            arr[i] = arr_temp.get(i);
+
+        }
+        return (arr);
+
+    }
+
+}
