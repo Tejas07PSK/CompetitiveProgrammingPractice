80c2e55bd28a2e1fc43e350d451e67cc78a00a5c
Palash Sarkar <palashsarkar0007@gmail.com>
2018-09-05 18:21:49 +0530

    LeetCode 'Arrays' problem 'Lnaltoo.java' (Largest Number At Least Twice Of Others) updated!!

--- InterviewBit/src/com/ib/programming/arrays/Lnaltoo.java
+++ InterviewBit/src/com/ib/programming/arrays/Lnaltoo.java
@@ -14,54 +14,26 @@ public class Lnaltoo
     public int dominantIndex(int[] nums)
     {
 
-        int large_idx = 0, i = 0, flag = 0, sim_large = 0;
-        for (i = 0; i < nums.length; i += 1)
+        int large_idx = 0, i = 0;
+        for (i = 0; i < nums.length; i++)
         {
 
-            if (nums[i] == nums[large_idx])
-            {
-
-                sim_large += 1;
-
-            }
             if (nums[i] > nums[large_idx])
             {
 
                 large_idx = i;
-                sim_large = 1;
 
             }
 
         }
-        for (i = 0; i < nums.length; i += 1)
-        {
-            if (large_idx == i)
-            {
-
-                continue;
-
-            }
-            if (nums[large_idx] >= (2 * nums[i]))
-            {
-
-                flag++;
-
-            }
-
-        }
-        //System.out.println(sim_large + " " + flag + " " + large_idx);
-        if (flag == (nums.length - sim_large))
+        for (i = 0; i < nums.length; i++)
         {
 
-            return (large_idx);
+            if ((i != large_idx) && (nums[large_idx] < (2 * nums[i])))
+                return (-1);
 
         }
-        else
-        {
-
-            return (-1);
-
-        }
+        return (large_idx);
 
     }
 
