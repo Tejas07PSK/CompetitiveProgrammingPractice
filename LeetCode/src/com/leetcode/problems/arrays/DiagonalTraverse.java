f34b22fd515b2469a4cef7a0320554a212ab1baf
Palash Sarkar <palashsarkar0007@gmail.com>
2018-09-07 20:59:47 +0530

    Fixed 'LeetCode' directory issue!!

---
+++ LeetCode/src/com/leetcode/problems/arrays/DiagonalTraverse.java
@@ -0,0 +1,128 @@
+/*
+
+        @Author : Tejas07PSK (Palash Sarkar),
+        @CreatedON : 07 Sep, 2018, 2:04 PM,
+        @File-Name : DiagonalTraverse.java
+
+ */
+
+package com.leetcode.problems.arrays;
+
+public class DiagonalTraverse
+{
+
+    public int[] findDiagonalOrder(int[][] matrix)
+    {
+
+        if ((matrix.length == 0) || ((matrix.length == 1) && (matrix[0].length == 0)))
+        {
+
+            return (new int [0]);
+
+        }
+        else if (matrix.length == 1)
+        {
+
+            return (matrix[0]);
+
+        }
+        else if (matrix[0].length == 1)
+        {
+
+            int[] res_arr = new int[matrix.length];
+            for (int i = 0; i < matrix.length; i += 1)
+            {
+
+                res_arr[i] = matrix[i][0];
+
+            }
+            return (res_arr);
+
+        }
+        int i = 0, j = 0, dir = 1, count = 0;
+        int res_arr [] = new int [(matrix.length * matrix[0].length)];
+        while ((i < matrix.length) && (j < matrix[0].length))
+        {
+
+            res_arr[count] = matrix[i][j];
+            count += 1;
+            if (dir == 1)
+            {
+
+                j += 1;
+                if (i == 0)
+                {
+
+                    dir = 2;
+
+                }
+                else
+                {
+
+                    dir = 4;
+                }
+
+            }
+            else if (dir == 2)
+            {
+
+                j -= 1;
+                i += 1;
+                if (j == 0)
+                {
+
+                    dir = 3;
+
+                }
+                if (i == (matrix.length - 1))
+                {
+
+                    dir = 1;
+
+                }
+
+            }
+            else if (dir == 3)
+            {
+
+                i += 1;
+                if (j == 0)
+                {
+
+                    dir = 4;
+
+                }
+                else
+                {
+
+                    dir = 2;
+
+                }
+
+            }
+            else // dir = 4
+            {
+
+                i -= 1;
+                j += 1;
+                if (i == 0)
+                {
+
+                    dir = 1;
+
+                }
+                if (j == (matrix[0].length - 1))
+                {
+
+                    dir = 3;
+
+                }
+
+            }
+
+        }
+        return (res_arr);
+
+    }
+
+}
---
+++ LeetCode/src/com/leetcode/problems/arrays/Lnaltoo.java
@@ -0,0 +1,41 @@
+/*
+
+        @Author : Tejas07PSK (Palash Sarkar),
+        @CreatedON : 05 Sep, 2018, 5:00 PM,
+        @File-Name : Lnaltoo.java
+
+ */
+
+package com.leetcode.problems.arrays;
+
+public class Lnaltoo
+{
+
+    public int dominantIndex(int[] nums)
+    {
+
+        int large = Integer.MIN_VALUE, seclarge = Integer.MIN_VALUE, i = 0, index = 0;
+        for (i = 0; i < nums.length; i++)
+        {
+
+            if (nums[i] > large)
+            {
+
+                seclarge = large;
+                large = nums[i];
+                index = i;
+
+            }
+            else if (nums[i] > seclarge)
+            {
+
+                seclarge = nums[i];
+
+            }
+
+        }
+        return ((large < (2 * seclarge)) ? -1 : index);
+
+    }
+
+}
