b9bb16f11b908ed85504ab6caf0d0236363ef236
Palash Sarkar <palashsarkar0007@gmail.com>
2018-09-08 21:54:43 +0530

    InterviewBit 'Arrays' problem 'AddOneToNumber.java' updated !!

--- InterviewBit/src/com/ib/programming/arrays/AddOneToNumber.java
+++ InterviewBit/src/com/ib/programming/arrays/AddOneToNumber.java
@@ -13,43 +13,31 @@ import java.util.Stack;
 public class AddOneToNumber
 {
 
-    public int[] plusOne(int[] number)
+    public int[] plusOne(final int[] A)
     {
 
-        if ((number == null) || (number.length == 0))
+        if ((A == null) || (A.length == 0))
         {
 
             return (new int [0]);
 
         }
-        if ((number[0] == 0) && (number.length == 1))
+        if ((A[0] == 0) && (A.length == 1))
         {
 
             int tmp [] = {1};
             return (tmp);
 
         }
-        Stack <Integer> tmp_lst = new Stack <Integer> ();
-        int i = 0, j = 0, z = 0, count = 0, new_num_len = 0, carry = 1;
-        int [] res_num = null;
-        while (true)
+        final Stack <Integer> stk = new Stack <Integer> ();
+        int i = 0, z = 0, carry = 1;
+        int [] res_no = null;
+        z = countZeroes(A);
+        for (i = (A.length - 1); i >= z; i -= 1)
         {
 
-            if (number[i] != 0)
-            {
-
-                break;
-
-            }
-            z += 1;
-            i += 1;
-
-        }
-        for (i = (number.length - 1); i >= z; i -= 1)
-        {
-
-            tmp_lst.push((number[i] + carry) % 10);
-            carry = (number[i] + carry) / 10;
+            stk.push((A[i] + carry) % 10);
+            carry = (A[i] + carry) / 10;
             if (carry == 0)
             {
 
@@ -62,26 +50,54 @@ public class AddOneToNumber
         if ((i == (z - 1)) && (carry != 0))
         {
 
-            tmp_lst.push(carry);
+            stk.push(carry);
 
         }
-        new_num_len = (i - z + 1) + tmp_lst.size();
-        res_num = new int [new_num_len];
+        res_no = new int [(i - z + 1) + stk.size()];
+        return (formNewNumber(A, res_no, stk, z, i));
+
+    }
+
+    private int countZeroes(final int A [])
+    {
+
+        int i = 0, z = 0;
+        while (true)
+        {
+
+            if (A[i] != 0)
+            {
+
+                break;
+
+            }
+            z += 1;
+            i += 1;
+
+        }
+        return (z);
+
+    }
+
+    private int [] formNewNumber(final int [] A, final int res_no [], final Stack <Integer> stk, int z, int i)
+    {
+
+        int j = 0, count = 0;
         for (j = z ; j <= i ; j += 1)
         {
 
-            res_num[count] = number[j];
+            res_no[count] = A[j];
             count += 1;
 
         }
-        while (!(tmp_lst.empty()))
+        while (!(stk.empty()))
         {
 
-            res_num[count] = tmp_lst.pop();
+            res_no[count] = stk.pop();
             count += 1;
 
         }
-        return (res_num);
+        return (res_no);
 
     }
 
