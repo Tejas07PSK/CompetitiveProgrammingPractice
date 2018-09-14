1c113753b2d120708ba1ccd55075d0e5b889c7d9
Palash Sarkar <palashsarkar0007@gmail.com>
2018-09-14 23:53:57 +0530

    InterviewBit 'Arrays' problem 'NobleInteger.java' !!

---
+++ InterviewBit/src/com/ib/programming/arrays/NobleInteger.java
@@ -0,0 +1,42 @@
+/*
+
+        @Author : Tejas07PSK (Palash Sarkar),
+        @CreatedON : 14 Sep, 2018, 9:49 PM,
+        @File-Name : NobleInteger.java
+
+ */
+
+package com.ib.programming.arrays;
+
+import java.util.Arrays;
+
+public class NobleInteger
+{
+
+    public int solve(final int A [])
+    {
+
+        int i = 0;
+        Arrays.sort(A);
+        for (i = 0; i < A.length; i += 1)
+        {
+
+            if ((A.length - (i + 1)) == A[i])
+            {
+
+                if (((i + 1) != A.length) && (A[i] == A[i + 1]))
+                {
+
+                    continue;
+
+                }
+                return (1);
+
+            }
+
+        }
+        return (-1);
+
+    }
+
+}
