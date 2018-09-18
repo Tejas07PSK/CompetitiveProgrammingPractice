6c74295f4206504bfaf810e7cf0503b2a921962f
Palash Sarkar <palashsarkar0007@gmail.com>
2018-09-18 23:47:02 +0530

    InterviewBit 'Arrays' problem 'WaveArray.java' updated!!

--- InterviewBit/src/com/ib/programming/arrays/WaveArray.java
+++ InterviewBit/src/com/ib/programming/arrays/WaveArray.java
@@ -9,32 +9,18 @@
 package com.ib.programming.arrays;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class WaveArray
 {
 
-    public ArrayList<Integer> wave(ArrayList<Integer> A)
+    public ArrayList<Integer> wave(final ArrayList<Integer> A)
     {
 
-        for (int i = 0; i < A.size(); i += 2)
+        for (int i = 0; i < (A.size() - 1); i += 2)
         {
 
-            if ((i > 0) && (A.get(i - 1) > A.get(i)))
-            {
-
-                A.set(i, (A.get(i) ^ A.get(i - 1)));
-                A.set((i - 1), (A.get(i) ^ A.get(i - 1)));
-                A.set(i, (A.get(i) ^ A.get(i - 1)));
-
-            }
-            if ((i < (A.size() - 1)) && (A.get(i + 1) > A.get(i)))
-            {
-
-                A.set(i, (A.get(i) ^ A.get(i + 1)));
-                A.set((i + 1), (A.get(i) ^ A.get(i + 1)));
-                A.set(i, (A.get(i) ^ A.get(i + 1)));
-
-            }
+            Collections.swap(A, i, (i + 1));
 
         }
         return (A);
