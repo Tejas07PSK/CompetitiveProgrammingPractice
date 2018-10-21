0981865aad7f2ac06b597a73ecee396ce26488a9
Palash Sarkar <palashsarkar0007@gmail.com>
2018-10-22 01:24:38 +0530

    Interviewbit 'Math' problem 'ExcelColumnNumber.java' updated!!

--- InterviewBit/src/com/ib/programming/math/ExcelColumnNumber.java
+++ InterviewBit/src/com/ib/programming/math/ExcelColumnNumber.java
@@ -12,15 +12,15 @@ public class ExcelColumnNumber
 {
 
     private static final int base = 26;
-    public int titleToNumber(final String col_id)
+    public int titleToNumber(final String A)
     {
 
-        int up = col_id.length() - 1;
+        int up = A.length() - 1;
         int sum = 0;
         for (int i = 0; i <= up; i += 1)
         {
 
-            sum += ((int)Math.pow(base, (up - i)) * ((int)col_id.charAt(i) - 64));
+            sum += ((int)Math.pow(base, (up - i)) * ((int)A.charAt(i) - 64));
 
         }
         return (sum);
